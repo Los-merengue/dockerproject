f6a609eeeb85de9524261614088348657925e255
Los-merengue <michaelsopara@gmail.com>
2022-04-01 16:53:27 +0000

    Configured kubernetes and ran pod

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=galactican/endpoint
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run endpoint-pod --image=$dockerpath --port=80 --labels app=endpoint-pod
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward endpoint-pod 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=galactican/endpoint
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=galactican --password-stdin
+docker tag endpoint $dockerpath
+
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
